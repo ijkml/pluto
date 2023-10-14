9eb64cb02afd5dbd783f55039f08417fba5b4e2b
ML <me.mlaure@gmail.com>
2023-10-14 17:40:19 +0100

    update unocss config

--- unocss.config.ts
+++ unocss.config.ts
@@ -1,6 +1,5 @@
 import {
   defineConfig,
-  presetAttributify,
   presetIcons,
   presetTypography,
   presetUno,
@@ -9,19 +8,27 @@ import {
 } from 'unocss';
 
 export default defineConfig({
-  shortcuts: [
-    [
-      'btn',
-      'px-4 py-1 rounded inline-block bg-teal-600 text-white cursor-pointer hover:bg-teal-700 disabled:cursor-default disabled:bg-gray-600 disabled:opacity-50',
-    ],
-    [
-      'icon-btn',
-      'inline-block cursor-pointer select-none opacity-75 transition duration-200 ease-in-out hover:opacity-100 hover:text-teal-600',
-    ],
-  ],
+  theme: {
+    colors: {
+      // ml: {
+      //   0: 'hsl(0, 100%, 59%)',
+      // },
+    },
+    fontFamily: {
+      // sans: [],
+    },
+    breakpoints: {
+      // 'xs': '360px',
+      // 'ss': '480px',
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    },
+  },
   presets: [
     presetUno(),
-    presetAttributify(),
     presetIcons({
       scale: 1.2,
     }),
